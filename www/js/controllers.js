2743564765d13dee282f55ad289776f1e7804dbb
wangxingmin <wangxingmin@misrobot.com>
2016-05-03 18:04:42 +0800

    删除一些无用代码

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,95 +23,7 @@ angular.module('app.controllers', ['ngCordova' ])
 .controller('page7Ctrl', function($scope) {
 
 })
-.controller('page8Ctrl', function($scope) {
-
-})
-.controller('CameraCtrl', function($scope,$cordovaCamera) {
-//-------------------------------
-    // 1
-    $scope.images = [];
-  //camera
-	$scope.addImage = function() {
-		// 2
-		var options = {
-			destinationType : Camera.DestinationType.FILE_URI,
-			sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-			allowEdit : false,
-			encodingType: Camera.EncodingType.JPEG,
-			popoverOptions: CameraPopoverOptions,
-		};
-		
-		// 3
-		$cordovaCamera.getPicture(options).then(function(imageData) {
-	 
-			// 4
-			onImageSuccess(imageData);
-	 
-			function onImageSuccess(fileURI) {
-				createFileEntry(fileURI);
-			}
-	 
-			function createFileEntry(fileURI) {
-				window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-			}
-	 
-			// 5
-			function copyFile(fileEntry) {
-				var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-				var newName = makeid() + name;
-	 
-				window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-					fileEntry.copyTo(
-						fileSystem2,
-						newName,
-						onCopySuccess,
-						fail
-					);
-				},
-				fail);
-			}
-			
-			// 6
-			function onCopySuccess(entry) {
-				$scope.$apply(function () {
-					$scope.images.push(entry.nativeURL);
-					//and storage
-					window.localStorage.setItem('images', JSON.stringify($scope.images));
-				});
-			}
-	 
-			function fail(error) {
-				console.log("fail: " + error.code);
-			}
-	 
-			function makeid() {
-				var text = "";
-				var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	 
-				for (var i=0; i < 5; i++) {
-					text += possible.charAt(Math.floor(Math.random() * possible.length));
-				}
-				return text;
-			}
-	 
-		}, function(err) {
-			console.log(err);
-		});
-	};
-	
-	$scope.urlForImage = function(imageName) {
-		var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-		var trueOrigin = cordova.file.dataDirectory + name;
-		return trueOrigin;
-	};
-	//刷新
-	$scope.$on('$ionicView.enter', function() {
-        if(window.localStorage.getItem("images")!=null)
-        {
-            var storedImgs = JSON.parse(window.localStorage.getItem("images"));
-            $scope.images =storedImgs;
-        } 
-    });
+.controller('pageSetCtrl', function($scope) {
 
 })
 
@@ -131,7 +43,6 @@ angular.module('app.controllers', ['ngCordova' ])
         });
     };
 
-
     $scope.urlForClipThumb = function(clipUrl) {
         var name = clipUrl.substr(clipUrl.lastIndexOf('/') + 1);
         var trueOrigin = cordova.file.dataDirectory + name;
@@ -142,13 +53,12 @@ angular.module('app.controllers', ['ngCordova' ])
     $scope.showClip = function(clip) {
         console.log('show clip: ' + clip);
     }
-
 })
 // 
 
    
 .controller('pageAboutCtrl', function($scope) {
-alert(1);
+
 })
 // 
 
