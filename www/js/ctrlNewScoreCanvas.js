f552bcd8dd8f0c00f053974d49e168c531814cf2
wangxingmin <wangxingmin@misrobot.com>
2016-05-23 16:17:21 +0800

    图形化界面加入记录按钮

--- www/js/ctrlNewScoreCanvas.js
+++ www/js/ctrlNewScoreCanvas.js
@@ -10,5 +10,6 @@ angular.module('app.controllers').controller('RecordNewCtrl3', function($scope) 
       context2D.drawImage( image,0,0,canvas.width,canvas.width);   
    };
    image.src = "img/60ba.jpg";
- //alert(x.innerHTML);
+   //alert(x.innerHTML);
+   canvas.height = canvas.width;
 })
